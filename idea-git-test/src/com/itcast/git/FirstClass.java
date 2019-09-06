c7cca1a6ee64d83853672b8eaa64bad5b65b0142
test <test20190906@163.com>
2019-09-06 17:47:39 +0800

    把本地工程添加到git

---
+++ idea-git-test/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git   11111");
+        System.out.println("hello git   22222");
+        System.out.println("hello git   33333");
+    }
+
+}
